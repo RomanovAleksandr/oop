a83d3e9cc5bb39cbfcb2130c2b91dbad59b724bc
RomanovAleksandr <61905953+RomanovAleksandr@users.noreply.github.com>
2020-04-02 14:40:29 +0300

    Update Decoder.cpp

--- lab2/HtmlDecode/HtmlDecode/Decoder.cpp
+++ lab2/HtmlDecode/HtmlDecode/Decoder.cpp
@@ -1,39 +1,44 @@
 #include "Decoder.h"
 
-std::string DecodeHtml(const std::string& html)
+std::map<std::string, std::string> htmlEntities{ 
+	{"&quot;", "\""}, 
+	{ "&apos;", "\'" }, 
+	{ "&lt;", "<" }, 
+	{ "&gt;", ">" }, 
+	{ "&amp;", "&" } };
+
+std::string ReplaceSubString(const std::string& line, const std::string& searchString, const std::string& replaceString)
 {
-	std::map<std::string, std::string> htmlEntities{ {"&quot;", "\""}, { "&apos;", "\'" }, { "&lt;", "<" }, { "&gt;", ">" }, { "&amp;", "&" } };
-	std::string decodeHtml;
+	std::string lineWithReplace;
 	size_t lastPos = 0;
 
-	while (true)
+	if (searchString != "")
 	{
-		size_t firstPos = std::string::npos;
-		std::pair<std::string, std::string> currentEntity = { "", "" };
-
-		for (std::pair<std::string, std::string> htmlEntity : htmlEntities)
+		lastPos = 0;
+		lineWithReplace = "";
+		size_t foundPos = line.find(searchString);
+		while (foundPos != std::string::npos)
 		{
-			size_t foundPos = html.find(htmlEntity.first, lastPos);
-			if (foundPos < firstPos)
-			{
-				firstPos = foundPos;
-				currentEntity = htmlEntity;
-			}
-		}
-
-		if (firstPos != std::string::npos)
-		{
-			decodeHtml.append(html, lastPos, firstPos - lastPos);
-			decodeHtml.append(currentEntity.second);
-			lastPos = firstPos + currentEntity.first.length();
-		}
-		else
-		{
-			break;
+			lineWithReplace.append(line, lastPos, foundPos - lastPos);
+			lineWithReplace.append(replaceString);
+			lastPos = foundPos + searchString.length();
+			foundPos = line.find(searchString, foundPos + searchString.length());
 		}
 	}
+	lineWithReplace.append(line, lastPos, line.length() - lastPos);
 
-	decodeHtml.append(html, lastPos, html.length() - lastPos);
+	return lineWithReplace;
+}
+
+std::string DecodeHtml(const std::string& html)
+{
+	
+	std::string decodeHtml = html;
+
+	for (std::pair<std::string, std::string> htmlEntity : htmlEntities)
+	{
+		decodeHtml = ReplaceSubString(decodeHtml, htmlEntity.first, htmlEntity.second);
+	}
 
 	return decodeHtml;
 }

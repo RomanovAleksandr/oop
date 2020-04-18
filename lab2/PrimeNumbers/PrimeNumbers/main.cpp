cb31e27c0b39655141853ac406f1586a6275efcf
RomanovAleksandr <61905953+RomanovAleksandr@users.noreply.github.com>
2020-04-19 01:37:55 +0300

    Update main.cpp

--- lab2/PrimeNumbers/PrimeNumbers/main.cpp
+++ lab2/PrimeNumbers/PrimeNumbers/main.cpp
@@ -6,41 +6,24 @@
 
 using namespace std;
 
-optional<int> parseArgs(int argc, char* argv[])
+int main(int argc, char* argv[])
 {
 	if (argc != 2)
 	{
 		cout << "Invalid arguments count\n"
 			<< "Usage: PrimeNumbers.exe <upper bound>\n";
-		return std::nullopt;
-	}
-
-	string upperBoundStr = argv[1];
-	size_t badPos = 0;
-	int upperBound = stoi(upperBoundStr, &badPos, 10);
-	if (badPos == upperBoundStr.length())
-	{
-		return upperBound;
-	}
-
-	return nullopt;
-}
-
-int main(int argc, char* argv[])
-{
-	auto upperBound = parseArgs(argc, argv);
-	if (!upperBound)
-	{
 		return 1;
 	}
 
-	if (upperBound.value() < 0)
+	int upperBound = atoi(argv[1]);
+	
+	if (upperBound < 0)
 	{
 		cout << "upper bound must be positive" << endl;
 		return 1;
 	}
 
-	set<int> primeNumbers = GeneratePrimeNumbersSet(upperBound.value());
+	set<int> primeNumbers = GeneratePrimeNumbersSet(upperBound);
 
 
 	for (int prime : primeNumbers)

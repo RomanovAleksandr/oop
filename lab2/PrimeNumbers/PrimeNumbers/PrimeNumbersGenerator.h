cd74011384725b0df049cf3d5a7cc21875bdee4c
RomanovAleksandr <61905953+RomanovAleksandr@users.noreply.github.com>
2020-04-18 23:40:38 +0300

    PrimeNumbersGenerator

--- lab2/PrimeNumbers/PrimeNumbers/PrimeNumbersGenerator.h
+++ lab2/PrimeNumbers/PrimeNumbers/PrimeNumbersGenerator.h
@@ -1,6 +1,5 @@
 #pragma once
 #include <set>
 #include <vector>
-#include <iostream>
 
 std::set<int> GeneratePrimeNumbersSet(const int upperBound);
--- lab2/PrimeNumbers/PrimeNumbers/main.cpp
+++ lab2/PrimeNumbers/PrimeNumbers/main.cpp
@@ -1,16 +1,48 @@
 #include <iostream>
 #include <set>
+#include <optional>
+#include <string>
 #include "PrimeNumbersGenerator.h"
 
 using namespace std;
 
+optional<int> parseArgs(int argc, char* argv[])
+{
+	if (argc != 2)
+	{
+		cout << "Invalid arguments count\n"
+			<< "Usage: PrimeNumbers.exe <upper bound>\n";
+		return std::nullopt;
+	}
+
+	string upperBoundStr = argv[1];
+	size_t badPos = 0;
+	int upperBound = stoi(upperBoundStr, &badPos, 10);
+	if (badPos == upperBoundStr.length())
+	{
+		return upperBound;
+	}
+
+	return nullopt;
+}
+
 int main(int argc, char* argv[])
 {
-	set<int> primeNumbers = GeneratePrimeNumbersSet(23);
-	if (primeNumbers.empty())
+	auto upperBound = parseArgs(argc, argv);
+	if (!upperBound)
 	{
-		cout << "empty";
+		return 1;
 	}
+
+	if (upperBound.value() < 0)
+	{
+		cout << "upper bound must be positive" << endl;
+		return 1;
+	}
+
+	set<int> primeNumbers = GeneratePrimeNumbersSet(upperBound.value());
+
+
 	for (int prime : primeNumbers)
 	{
 		cout << prime << ' ';

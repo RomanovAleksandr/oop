1bae7510acdfad2e2bc0ef444d981be0e02a8089
RomanovAleksandr <61905953+RomanovAleksandr@users.noreply.github.com>
2020-05-06 17:25:19 +0300

    Complex

---
+++ lab5/Complex/Complex/CComplex.cpp
@@ -0,0 +1,2 @@
+#include "CComplex.h"
+
---
+++ lab5/Complex/Complex/CComplex.h
@@ -0,0 +1,14 @@
+#pragma once
+
+class CComplex
+{
+public:
+	CComplex(double real = 0, double image = 0);
+	double Re()const;
+	double Im()const;
+	double GetMagnitude()const;
+	double GetArgument()const;
+
+private:
+
+};

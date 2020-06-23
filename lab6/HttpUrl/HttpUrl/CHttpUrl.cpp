ad4524466bed2ee20fe9584f166740cc2fe04649
RomanovAleksandr <61905953+RomanovAleksandr@users.noreply.github.com>
2020-06-24 01:54:09 +0300

    HttpUrl

---
+++ lab6/HttpUrl/HttpUrl/CHttpUrl.cpp
@@ -0,0 +1,8 @@
+#include "CHttpUrl.h"
+
+using namespace std;
+
+CHttpUrl::CHttpUrl(string const& url)
+{
+
+}
---
+++ lab6/HttpUrl/HttpUrl/CHttpUrl.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <string>
+
+enum Protocol
+{
+	HTTP,
+	HTTPS
+};
+
+class CHttpUrl
+{
+public:
+	CHttpUrl(std::string const& url);
+	//~CHttpUrl();
+	CHttpUrl(
+		std::string const& domain,
+		std::string const& document,
+		Protocol protocol = HTTP);
+	
+	CHttpUrl(
+		std::string const& domain,
+		std::string const& document,
+		Protocol protocol,
+		unsigned short port);
+
+	std::string GetURL()const;
+	std::string GetDomain()const;
+	std::string GetDocument()const;
+	Protocol GetProtocol()const;
+	unsigned short GetPort()const;
+
+
+private:
+	std::string domain;
+	std::string document;
+	Protocol protocol;
+	unsigned short port;
+};
